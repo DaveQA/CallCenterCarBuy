97e9ea081c7822bcd11ca19545dd950c6f9b4290
dholliday <asd>
2021-09-21 16:58:53 -0400

    Number of changes Added doesVehicleRun yes and no methods Updated clickNextButton method with wait methods Updated car parts functions to address the new waits and workflow

--- src/test/java/quoteWorkFlow.java
+++ src/test/java/quoteWorkFlow.java
@@ -21,6 +21,31 @@ public class quoteWorkFlow {
         utilities.clickNextButton();
     }
 
+    public static void fillingOutSecondScreen(WebDriver driver1) throws InterruptedException {
+        driver = driver1;
+        utilities.enteringZipCodeStatic();
+        utilities.clickNextButton();
+        utilities.pickingYearOfCarStatic();
+        utilities.pickingMakeOfCarStatic();
+        utilities.pickingModelOfCarStatic();
+        utilities.clickNextButton();
+        utilities.clickingNoRadioButtonIsThereDamageToCar();
+        utilities.clickNextButton();
+        utilities.pickingCarPartEngine();
+        utilities.pickingCarPartTransmission();
+        utilities.clickNextButton();
+    }
+
+    public static void fillingOutQuoteOfferScreen(WebDriver driver1) throws InterruptedException {
+        driver = driver1;
+        WebElement scrollToTopOfWizard = driver.findElement(By.cssSelector("#app > div > div.page-wrapper-row.full-height > div > div > div > div.page-content > div > div > div > div > div > div > div.todo-content > div > div.portlet-title > div.caption"));
+        String scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
+                + "var elementTop = arguments[0].getBoundingClientRect().top;"
+                + "window.scrollBy(0, elementTop-(viewPortHeight/2));";
+        ((JavascriptExecutor) driver).executeScript(scrollElementIntoMiddle, scrollToTopOfWizard);
+        utilities.clickNextButton();
+    }
+
     public static void gettingToCarPartListContainer(WebDriver driver1) throws InterruptedException {
         driver = driver1;
         quoteWorkFlow.fillingOutFirstScreen(driver);
@@ -34,19 +59,10 @@ public class quoteWorkFlow {
         utilities.clickNextButton();
     }
 
-    public static void gettingToDoesVehicleRunQuestionScreen(WebDriver driver1) throws InterruptedException {
+    public static void gettingToDoesTheVehicleRunQuestion(WebDriver driver1) throws InterruptedException {
         driver = driver1;
-        quoteWorkFlow.gettingToCarPartListContainer(driver);
-        utilities.pickingCarPartEngine();
-        utilities.pickingCarPartTransmission();
-        utilities.clickNextButton();
-                Thread.sleep(1000);
-        WebElement scrollToTopOfWizard = driver.findElement(By.cssSelector("#app > div > div.page-wrapper-row.full-height > div > div > div > div.page-content > div > div > div > div > div > div > div.todo-content > div > div.portlet-title > div.caption"));
-        String scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
-                + "var elementTop = arguments[0].getBoundingClientRect().top;"
-                + "window.scrollBy(0, elementTop-(viewPortHeight/2));";
-        ((JavascriptExecutor) driver).executeScript(scrollElementIntoMiddle, scrollToTopOfWizard);
-                Thread.sleep(1000);
-        utilities.clickNextButton();
+        fillingOutFirstScreen(driver);
+        fillingOutSecondScreen(driver);
+        fillingOutQuoteOfferScreen(driver);
     }
 }
