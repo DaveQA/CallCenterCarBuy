908b4638db88ac115248083c271ad57b165a20a8
DaveQA <34749850+DaveQA@users.noreply.github.com>
2022-03-15 12:47:47 -0400

    Started updating project

--- src/test/java/utilities.java
+++ src/test/java/utilities.java
@@ -36,6 +36,11 @@ public class utilities {
         waitOnBothErrorMessageAndLoadingGif();
     }
 
+    public static void clickSkipButton(){
+        waitOnBothErrorMessageAndLoadingGif();
+        driver.findElement(By.linkText("Skip")).click();
+    }
+
     public static void waitOnButtonToBeClickable(By by){
         WebDriverWait waitOnButtonToBeClickable = new WebDriverWait(driver,30);
         waitOnButtonToBeClickable.until(ExpectedConditions.elementToBeClickable(by));
@@ -114,40 +119,6 @@ public class utilities {
     public static void clickNewQuoteButton() throws InterruptedException {
         clickSomething(By.cssSelector("#pro-wizard > div:nth-child(1) > div > div:nth-child(2) > div > div:nth-child(3) > button"));
                 Thread.sleep(1000);
-    }
-
-    public static String randomizeFirstName() {
-        String[] firstNameOptions;
-        firstNameOptions = new String[10];
-        firstNameOptions[0] = "FirstName1";
-        firstNameOptions[1] = "FirstName2";
-        firstNameOptions[2] = "FirstName3";
-        firstNameOptions[3] = "FirstName4";
-        firstNameOptions[4] = "FirstName5";
-        firstNameOptions[5] = "FirstName6";
-        firstNameOptions[6] = "FirstName7";
-        firstNameOptions[7] = "FirstName8";
-        firstNameOptions[8] = "FirstName9";
-        firstNameOptions[9] = "FirstName10";
-        int randomFirstName = (int) (Math.random() * firstNameOptions.length);
-        return firstNameOptions[randomFirstName];
-    }
-
-    public static String randomizeLastName() {
-        String[] lastNameOptions;
-        lastNameOptions = new String[10];
-        lastNameOptions[0] = "LastName1";
-        lastNameOptions[1] = "LastName2";
-        lastNameOptions[2] = "LastName3";
-        lastNameOptions[3] = "LastName4";
-        lastNameOptions[4] = "LastName5";
-        lastNameOptions[5] = "LastName6";
-        lastNameOptions[6] = "LastName7";
-        lastNameOptions[7] = "LastName8";
-        lastNameOptions[8] = "LastName9";
-        lastNameOptions[9] = "LastName10";
-        int randomLastName = (int) (Math.random() * lastNameOptions.length);
-        return lastNameOptions[randomLastName];
     }
 
     public static void clickingYesRadioButtonIsThereDamageToCar(){
@@ -465,6 +436,40 @@ public class utilities {
                 driver.findElement(By.cssSelector("#pro-wizard > div:nth-child(1) > div > div.form-group > div > div:nth-child(2) > header > span.next")).click();
             }
         }
+    }
+
+    public static String randomizeFirstName() {
+        String[] firstNameOptions;
+        firstNameOptions = new String[10];
+        firstNameOptions[0] = "FirstName1";
+        firstNameOptions[1] = "FirstName2";
+        firstNameOptions[2] = "FirstName3";
+        firstNameOptions[3] = "FirstName4";
+        firstNameOptions[4] = "FirstName5";
+        firstNameOptions[5] = "FirstName6";
+        firstNameOptions[6] = "FirstName7";
+        firstNameOptions[7] = "FirstName8";
+        firstNameOptions[8] = "FirstName9";
+        firstNameOptions[9] = "FirstName10";
+        int randomFirstName = (int) (Math.random() * firstNameOptions.length);
+        return firstNameOptions[randomFirstName];
+    }
+
+    public static String randomizeLastName() {
+        String[] lastNameOptions;
+        lastNameOptions = new String[10];
+        lastNameOptions[0] = "LastName1";
+        lastNameOptions[1] = "LastName2";
+        lastNameOptions[2] = "LastName3";
+        lastNameOptions[3] = "LastName4";
+        lastNameOptions[4] = "LastName5";
+        lastNameOptions[5] = "LastName6";
+        lastNameOptions[6] = "LastName7";
+        lastNameOptions[7] = "LastName8";
+        lastNameOptions[8] = "LastName9";
+        lastNameOptions[9] = "LastName10";
+        int randomLastName = (int) (Math.random() * lastNameOptions.length);
+        return lastNameOptions[randomLastName];
     }
 }
 
